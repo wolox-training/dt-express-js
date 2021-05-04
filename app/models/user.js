daf399872312c37ec2c837cd21bd73d46a2b8284
danielWolox <82824773+danielWolox@users.noreply.github.com>
2021-05-04 15:55:21 -0500

    User model and migration (#2)
    Co-authored-by: Alejandro Tamayo <alejandrotamayo@MacBook-Pro-de-WOLOX.local>

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'user',
+    {
+      firstName: { type: DataTypes.STRING, allowNull: false },
+      lastName: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false, unique: true },
+      password: { type: DataTypes.STRING, allowNull: false }
+    },
+    {
+      tableName: 'users',
+      underscored: true
+    }
+  );
+  return User;
+};
